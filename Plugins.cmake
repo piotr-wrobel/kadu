eeaefa715f776c3f97009bc27859143706e8c745
wrobepio <piotr.wrobel.stanislaw@gmail.com>
2024-03-26 16:03:04 +0100

    Disabled Ubuntu integration

--- Plugins.cmake
+++ Plugins.cmake
@@ -108,12 +108,12 @@ if (UNIX AND NOT APPLE)
 	# docking
 		# Indicator docking support
 		# Comment if you are not compilign under Ubuntu flavor
-		indicator_docking
+		#indicator_docking
 
 	# integration
 		# integration with Unity
 		# Comment if you are not compilign under Ubuntu flavor
-		unity_integration
+		#unity_integration
 
 	# notifiers
 		# Freedesktop notification support
